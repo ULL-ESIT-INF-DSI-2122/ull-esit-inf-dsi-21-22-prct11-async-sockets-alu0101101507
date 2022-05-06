036d5b96966da02f22fc79d39800d3417248ce0d
Jos'Miguel Hernández Santana <alu010110150@ull.edu.es>
2022-05-06 12:39:15 +0000

    Actu

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export function add(valor1: number, valor2:number): number {
+  return valor1 + valor2;
+}
+console.log(add(5, 6));
---
+++ tests/index.spec.ts
@@ -0,0 +1,9 @@
+import 'chai';
+import {expect} from 'chai';
+import {add} from '../src/index';
+
+describe('add function tests', () => {
+  it('add(1, 8) returns value 9', () => {
+    expect(add(1, 8)).to.be.equal(9);
+  });
+}); 
